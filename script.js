4a6a2cebd5b76c8a8b8a024c70b82b7659763ebb
Sylwia <sylwia.pawelec@wp.pl>
2017-11-19 15:08:01 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var a = 3;
+var b = 5;
+var operation = (a * a) - (2 * a * b) + (b * b);
+
+console.log(operation);
+
+var result = operation > 0 ? 'Wynik dodatni' : (operation < 0 ? 'Wynik ujemny' : 'Równy 0');
+
+console.log(result);
+
+    
